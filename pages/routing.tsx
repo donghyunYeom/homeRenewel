f7ac56904bfaf3ff912309f409118319f3d13d00
DongHyunYeom <sunderpower@naver.com>
2023-07-12 14:39:46 +0900

    Feat: Start Project Renewel home

---
+++ pages/routing.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Routing: NextPage = () => {
+    return (
+        <h1>
+            helpehelpehelpehlep
+        </h1>
+    );
+};
+
+export default Routing;
