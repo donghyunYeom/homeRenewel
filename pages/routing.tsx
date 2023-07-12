ae5b3fdbb5a678a84cdbd0c4d5432ebcdafa494b
DongHyunYeom <sunderpower@naver.com>
2023-07-12 14:52:17 +0900

    Feat: ass tailwind css

--- pages/routing.tsx
+++ pages/routing.tsx
@@ -2,9 +2,15 @@ import type { NextPage } from 'next'
 
 const Routing: NextPage = () => {
     return (
+        <>
         <h1>
             help page start
         </h1>
+
+        <div className="text-[20px] font-bold mb-[10px]">
+            tailwind css ++
+        </div>
+        </>
     );
 };
 
