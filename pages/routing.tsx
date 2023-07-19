6d073f6bfcf7098e1cf2814b90c2e3ecab2561fe
DongHyunYeom <sunderpower@naver.com>
2023-07-19 09:30:53 +0900

    Fix: ..

--- pages/routing.tsx
+++ pages/routing.tsx
@@ -3,13 +3,9 @@ import type { NextPage } from 'next'
 const Routing: NextPage = () => {
     return (
         <>
-        <h1>
-            help page start
-        </h1>
-
-        <div className="text-[20px] font-bold mb-[10px]">
-            tailwind css ++
-        </div>
+            <div className="sticky py-[200px]">
+                <div>안녕하세요</div>
+            </div>
         </>
     );
 };
